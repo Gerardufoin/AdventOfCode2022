4f382272467cc6b12caf7ce9c5b081b16b36a5f6
Gerardufoin <gerardufoin2@hotmail.com>
2022-12-01 20:35:27 +0100

    Reduced day01

--- day01/day01.ts
+++ day01/day01.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const input = fs.readFileSync('./input.txt').toString().replace(/\n$/s, '');
-const calories = input.toString().split('\n\n').map((a) => a.split('\n').reduce((r, v) => r + parseInt(v), 0)).sort((a, b) => a - b);
+const calories = input.split('\n\n').map((a) => a.split('\n').reduce((r, v) => r + parseInt(v), 0)).sort((a, b) => a - b);
 
 console.log(calories.at(-1));
-console.log((calories.at(-1) ?? 0) + (calories.at(-2) ?? 0) + (calories.at(-3) ?? 0));
+console.log(calories.slice(-3).reduce((r, v) => r + v, 0));
