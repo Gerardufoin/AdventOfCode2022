43654b4c5b0c8618f97a03eaf592c13b992d091c
Gerardufoin <gerardufoin2@hotmail.com>
2022-12-03 00:15:16 +0100

    Day02

---
+++ day02/day02.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+function getScoreP1(n: number[]): number {
+    return n[1] + (n[0] === n[1] ? 3 : (n[0] % 3 + 1) === n[1] ? 6 : 0);
+}
+
+function getScoreP2(n: number[]): number {
+    return ((n[1] - 1) * 3) + (n[0] + n[1]) % 3 + 1;
+}
+
+const input = fs.readFileSync('./input.txt').toString().replace(/A|X/g, '1').replace(/B|Y/g, '2').replace(/C|Z/g, '3');
+const rounds = input.split('\n').filter((v) => v.length > 0).map((v) => v.split(' ').map((n) => parseInt(n)));
+console.log(rounds.map((v) => getScoreP1(v)).reduce((r, v) => r + v, 0));
+console.log(rounds.map((v) => getScoreP2(v)).reduce((r, v) => r + v, 0));
